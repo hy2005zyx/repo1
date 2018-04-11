e975d82006460c3aa619ac9c11e431811400f680
liaoyan <liaoyan@chinasofti.com>
2018-04-11 16:52:40 +0800

    1

--- javase-demo/src/com/ly/Test.java
+++ javase-demo/src/com/ly/Test.java
@@ -4,9 +4,20 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		char a = '\u1111';
+		float f1 = 16777215f;  
+		for (int i = 0; i < 10; i++) {  
+		    System.out.println(f1);  
+		    f1++;  
+		}  
 		
-		System.out.println(a);
+		/*double d = (double) 2.2f;  
+		System.out.println(d);   
+		d = (double) 2.25f;  
+		System.out.println(d);   */
+		
+		System.out.println(2.9e-3f);
+
+
 
 
 	}
